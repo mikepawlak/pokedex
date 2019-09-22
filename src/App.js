0cc7226a4ff1bce402140cfc5f69cc2420fbcb92
Mike <mpawlak719@gmail.com>
2019-09-22 17:26:55 -0500

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import { ApolloProvider, useQuery } from "@apollo/react-hooks";
+import { ApolloClient, gql, InMemoryCache, HttpLink } from "apollo-boost";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://poke.mikepawlak.com"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+const ALL_POKEMON = gql(`{
+  pokemon {
+    name id sprites { front_default }
+  }
+}`);
+
+function PokemonList() {
+  const { loading, error, data } = useQuery(ALL_POKEMON);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.pokemon.map(({ name, id, sprites }) => (
+    <div key={id}>
+      <p>{name}</p>
+      <img src={sprites.front_default} alt={name} />
+    </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div>
+        <h2>My first Apollo app 🚀</h2>
+        <PokemonList />
+      </div>
+    </ApolloProvider>
   );
 }
 
